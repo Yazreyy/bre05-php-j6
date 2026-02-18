85011212966614aed23fb121cda0b32cb8476c4c
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-02-18 11:44:13 +0100

    etape 5

--- get_users_with_address.php
+++ get_users_with_address.php
@@ -1,7 +1,7 @@
 <?php
 require 'connexion.php';
 
-$query = $db->prepare('SELECT users.id, address.city, address.street, address.zipcode FROM users JOIN address ON users.id = address.id WHERE users.id = :id');
+$query = $db->prepare('SELECT users.id, users.first_name, users.last_name,users.address, address.city, address.street, address.zipcode FROM users JOIN address ON users.address = address.id WHERE users.id = :id');
 $parameters = [
     'id' => $_GET['id']
 ];
